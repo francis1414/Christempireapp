a7e4825336a7470e49cc266247e813222fb8b0b6
francis1414 <150382997+francis1414@users.noreply.github.com>
2025-07-19 20:43:47 +0000

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,13 +8,6 @@ const app = express();
 app.use(cors({ origin: '*' }));
 app.use(express.json());
 
-const express = require('express');
-const app = express();
-const path = require('path');
-
-// Serve static files from the public directory
-app.use(express.static(path.join(__dirname, 'public')));
-
 
 const PORT = process.env.PORT || 5000;
 
